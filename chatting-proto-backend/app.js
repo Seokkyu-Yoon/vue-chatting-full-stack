554d18313d9c5740e27afa9f0d8e2daa462a93b4
윤석규 <yskyu93@modutech.co.kr>
2021-01-08 12:07:52 +0900

    fix(render): add ejs module against to backend

--- chatting-proto-backend/app.js
+++ chatting-proto-backend/app.js
@@ -3,11 +3,14 @@ import path from 'path'
 import cookieParser from 'cookie-parser'
 import logger from 'morgan'
 import cors from 'cors'
+import ejs from 'ejs'
 
 const app = express()
 
 // view engine setup
 app.set('views', path.join(__dirname, 'public'))
+app.engine('html', ejs.renderFile)
+app.set('view engine', 'ejs')
 
 // CORS
 app.use(cors())
