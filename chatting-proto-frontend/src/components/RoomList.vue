57a400acfae9dd6509c2944742d72cbfc72944a7
윤석규 <yskyu93@modutech.co.kr>
2021-01-11 13:50:53 +0900

    refactor(room-list): make roomList card

--- chatting-proto-frontend/src/components/RoomList.vue
+++ chatting-proto-frontend/src/components/RoomList.vue
@@ -1,7 +1,6 @@
 <template>
   <div class="list-group flex-fill overflow-auto">
-
-    <b-card-group columns>
+    <b-card-group columns v-if="store.rooms.length > 0">
       <b-card
         v-show="store.rooms.length > 0"
         v-for="(room, idx) in store.rooms"
@@ -9,81 +8,40 @@
         :title="room.roomName"
         :img-src='"https://picsum.photos/200/100?random=" + idx'
         img-alt="Image"
-        img-top
-      >
-      <b-card-text>
-        {{ room.roomDesc }}
-      </b-card-text>
-
-      <div class="form-inline">
-        <b-btn size="sm">{{room.roomPassword ? '비밀방' : '공개방'}}</b-btn>
-        <!-- <b-form-input v-if="room.roomPassword" placeholder="비밀번호를 입력해주세요" v-model="password[room.roomKey]"/> -->
+        img-top>
         <b-btn
+          class="mt-1 mr-1 delete-room"
           size="sm"
-          class="ml-1"
-          v-on:click.stop="(e) => setCurrRoom(room)"> 참가 </b-btn>
-          <b-btn
-          size="sm"
-          class="ml-1 btn-danger"
-          v-visible="store.userName === room.createBy"
+          variant="danger"
+          v-show="store.userName === room.createBy"
           v-on:click.stop="() => deleteRoom(room.roomKey)">
           X
         </b-btn>
-      </div>
-
-      <b-list-group flush>
-      <b-list-group-item>
-        {{ room.joining }}
-      </b-list-group-item>
-    </b-list-group>
-
-    </b-card>
-    </b-card-group>
-    <!--
-    <div
-      class="list-group-item"
-      v-show="store.rooms.length > 0"
-      v-for="room in store.rooms"
-      v-bind:key="room.roomKey"
-      v-on:click="(e) => {
-
-      }"
-      >
-      <div class="d-flex justify-content-between">
-        <div class="d-flex flex-column justify-content-between align-items-start">
-          <h4 class="mt-1">{{room.roomName}}</h4>
-          <span class="badge badge-dark">
-            {{room.roomPassword ? '비밀방' : '공개방'}}
-          </span>
-          <span class="badge badge-dark text-center mt-1">
-            {{`${room.joining} / ${room.roomMaxJoin || '∞'}`}}
-          </span>
-        </div>
-        <div class="d-flex flex-column justify-content-between align-items-end">
-          <button
-            type="button"
-            class="btn btn-sm btn-danger"
-            v-visible="store.userName === room.createBy"
-            v-on:click.stop="() => deleteRoom(room.roomKey)">
-            X
-          </button>
-
-          <div class="form-inline">
-            <input type="password" class="form-control" v-visible="room.roomPassword" placeholder="비밀번호를 입력해주세요" v-model="password[room.roomKey]"/>
-            <button
-              type="button"
-              class="btn btn-sm btn-primary ml-1"
+        <div class="d-flex flex-column align-items-start">
+          <p class="h5 mb-1">
+            <span v-bind:class="getClassBadgeSecret(room)">
+              {{room.roomPassword ? '비밀방' : '공개방'}}
+            </span>
+            <span v-bind:class="getClassBadgeMaxJoin(room)">
+              {{`${room.joining} / ${room.roomMaxJoin || '∞'}`}}
+            </span>
+          </p>
+          <b-card-text>{{room.roomDesc}}</b-card-text>
+          <div class="w-100 d-flex justify-content-end">
+            <b-btn
+              class='ml-auto'
+              variant="primary"
+              size="sm"
               v-on:click.stop="(e) => setCurrRoom(room)">
               참가
-            </button>
+            </b-btn>
           </div>
         </div>
-      </div>
+      </b-card>
+    </b-card-group>
+    <div v-else>
+      <p class="h1">존재하는 방이 없습니다</p>
     </div>
-    <div v-show="store.rooms.joining === 0">
-      방이 없습니다
-    </div>
-    -->
   </div>
 </template>
 
@@ -94,4 +52,10 @@
 .list-group-item {
   cursor: pointer;
 }
+.delete-room {
+  position: absolute;
+  top: 0;
+  right: 0;
+}
+
 </style>
