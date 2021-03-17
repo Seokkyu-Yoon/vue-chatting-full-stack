241e59a5e2142aab754028989e1eec73c0fc8b19
윤석규 <yskyu93@modutech.co.kr>
2021-03-17 17:33:12 +0900

    fix(initDatabase): fix create database

--- chatting-proto-backend/src/plugins/mysql/init-database.js
+++ chatting-proto-backend/src/plugins/mysql/init-database.js
@@ -4,7 +4,7 @@ import { ConfigMysql } from '../../config'
 async function initDatabase () {
   const connection = new Connection({ ...ConfigMysql, database: undefined })
   await connection.query({
-    sql: `CREATE DATABASE IF NOT EXISTS ${ConfigMysql.database}`,
+    sql: `CREATE DATABASE IF NOT EXISTS \`${ConfigMysql.database}\``,
     params: []
   })
 }
